2c44ace203b76067180622f29930162b21ba7b7a
joyce-caroline <caroline.joyce5631@gmail.com>
2023-11-07 22:49:35 -0300

    Feat(main.js): Adiciona update e delete

--- main.js
+++ main.js
@@ -3,28 +3,40 @@ const app = express()
 const db = require("./db")
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 
-// Exercicio de CRUD
-// Utilizando as 5 funções encontradas em db, crie 5 endpoints para o recurso "usuario".
-// (Leia em README para saber mais sobre as funções)
-/* 
-    O recurso usuario deve ter as seguintes propriedades com seus respectivos tipos:
-    { 
-        name: String, 
-        email: String, 
-        password: String 
-    }
-*/
+
 app.get('/', function(req, res){
   const users = db.findAll();
   console.log(users);
   res.send(users);
 })
 
+app.get('/:id/:nome', function(req, res){
+  const userById = db.findById(req.params.id);
+  // console.log(req.params);
+  res.send(userById);
+});
+
+app.get('/user?', function(req,res){
+  const userWithQuery = db.findById(req.query.id);
+  console.log(req.query)
+  res.send(userWithQuery);
+});
+
 app.post('/', function(req, res){
   const newUser =  db.create(req.body);
-  res.send("Usuário Cadastrado com sucesso!");
+  res.status(201).send("Usuário Cadastrado com sucesso!");
+});
+
+app.put('/:id', function(req, res){
+  const upUser = db.updateById(req.params.id, req.body);
+  res.send(upUser);
+});
+
+app.delete('/:id', function(req, res){
+  const deleteUser = db.remove(req.params.id);
+  res.send(`Removido: ${deleteUser.message}`);
 });
 
 
